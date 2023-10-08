b41fd874890758b936f1a9690bcd8a44853e4d99
7777-nuriddin <ismatullayevnuriddin7@gmail.com>
2023-10-08 23:22:26 +0500

    IF

---
+++ homework_26/js/main.js
@@ -0,0 +1,31 @@
+
+// let a = parseInt(prompt("son kiriting"))
+
+// if (a > 0) {
+//     alert(a + 1)
+// }else{
+//     alert(a)
+// }
+
+
+
+
+
+// let a = parseInt(prompt("son kiriting"))
+
+// if (a > 0) {
+//     alert(a + 1)
+// }else{
+//     alert(a - 2)
+// }
+
+
+let a = parseInt(prompt("son kiriting"))
+
+if (a > 0) {
+    alert(a + 1)
+}else if (a < 0){
+    alert(a - 2)
+}else{
+    alert(10)
+}
