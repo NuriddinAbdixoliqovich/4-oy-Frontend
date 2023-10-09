65b2c7eca3342dd11e8f3211d5736ad405f2c9bd
7777-nuriddin <ismatullayevnuriddin7@gmail.com>
2023-10-09 10:14:34 +0500

    intial

---
+++ homework_25/js/main.js
@@ -0,0 +1,128 @@
+
+
+
+// Kvadratning perimetrini aniqlash
+
+// const a = parseInt(prompt("To'rtburchak tomonini kiriting"));
+
+
+// const P = 4 * a;
+
+// alert(P);
+
+
+
+
+// Kvadrat yuzini aniqlash
+
+
+// const b = parseInt(prompt("To'rtburchak tomonini kiriting"))
+
+// const S = b * b;
+
+// alert(S);
+
+
+
+
+
+
+
+// Togri tortburchakning yuzini va perimetrini aniqlash
+
+// const a = parseInt(prompt("To'rtburchak enini kiriting"));
+// const b = parseInt(prompt("To'rtburchak bo'yini kiriting"));
+
+// const S = a * b;
+// const P = 2 * (a+b);
+
+// alert(S);
+// alert(P);
+
+
+
+
+
+
+
+// Aylana uzunligini aniqlash
+
+// const d = parseInt(prompt("Aylana diametrini kiriting"));
+
+// const pi = 3.14;
+
+// const L = pi * d;
+
+// alert(L);
+
+
+
+
+
+// Kubning hajmi va tola sirtini topish
+
+
+
+// const a = parseInt(prompt("Kubning tomoni"));
+
+// const V = a * a * a;
+
+// const S = 6 * a * a;
+
+// alert(V);
+// alert(S);
+
+
+
+
+
+
+
+
+
+// Parallelipiped hajmi va tola sirti
+
+// const a = parseInt(prompt("birinchi tomoni"));
+// const b = parseInt(prompt("ikkinchi tomoni"));
+// const c = parseInt(prompt("uchinchi tomoni"));
+
+// const V = a * b * c;
+
+// const S = 2*(a*b+b*c+a*c);
+
+// alert(V);
+// alert(S);
+
+
+
+
+
+// Doiraning uzunligi va yuzasini aniqlash
+
+// const R = parseInt(prompt("Doira radiusi"));
+
+// const pi = 3.14;
+
+// const S = pi * R * R;
+// const L = 2 * pi * R;
+
+// alert(S);
+// alert(L);
+
+
+
+
+
+
+
+
+// Orta arifmetik qiymati
+
+
+
+const a = parseInt(prompt("birinchi son"));
+const b = parseInt(prompt("ikkinchi son"));
+
+const M = (a + b) / 2;
+
+alert(M);
