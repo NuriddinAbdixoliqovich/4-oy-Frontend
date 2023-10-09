cae373ce0f1ff4a16255204f384060bebe111677
7777-nuriddin <ismatullayevnuriddin7@gmail.com>
2023-10-09 10:08:00 +0500

    homework_26

--- homework_26/js/main.js
+++ homework_26/js/main.js
@@ -20,12 +20,17 @@
 // }
 
 
-let a = parseInt(prompt("son kiriting"))
+// let a = parseInt(prompt("son kiriting"))
 
-if (a > 0) {
-    alert(a + 1)
-}else if (a < 0){
-    alert(a - 2)
-}else{
-    alert(10)
-}
+// if (a > 0) {
+//     alert(a + 1)
+// }else if (a < 0){
+//     alert(a - 2)
+// }else{
+//     alert(10)
+// }
+
+
+let a = parseInt(prompt("birinchi sonni kiriting"))
+let b = parseInt(prompt("ikkinchi sonni kiriting"))
+let c = parseInt(prompt("uchinchi sonni kiriting"))
