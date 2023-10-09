e90e49e7133bf8d8118440ccc12ad0f61f5fb9a9
7777-nuriddin <ismatullayevnuriddin7@gmail.com>
2023-10-09 17:44:44 +0500

    if

--- homework_26/js/main.js
+++ homework_26/js/main.js
@@ -20,6 +20,11 @@
 // }
 
 
+
+
+
+
+
 // let a = parseInt(prompt("son kiriting"))
 
 // if (a > 0) {
@@ -31,6 +36,187 @@
 // }
 
 
-let a = parseInt(prompt("birinchi sonni kiriting"))
-let b = parseInt(prompt("ikkinchi sonni kiriting"))
-let c = parseInt(prompt("uchinchi sonni kiriting"))
+
+
+
+
+
+
+
+// let a = parseInt(prompt("birinchi sonni kiriting"));
+// let b = parseInt(prompt("ikkinchi sonni kiriting"));
+// let c = parseInt(prompt("uchinchi sonni kiriting"));
+
+
+// if (a > 0){
+//     console.log(countera = 1)
+// }else{
+//     console.log(countera = 0)
+// }
+
+// if (b > 0){
+//     console.log(counterb = 1)
+// }else{
+//     console.log(counterb = 0)
+// }
+
+// if (c > 0){
+//     console.log(counterc = 1)
+// }else{
+//     console.log(counterc = 0)
+// }
+
+// alert(counterd = countera + counterb + counterc)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// let a = parseInt(prompt("birinchi sonni kiriting"));
+// let b = parseInt(prompt("ikkinchi sonni kiriting"));
+// let c = parseInt(prompt("uchinchi sonni kiriting"));
+
+
+// if (a > 0){
+//     console.log(countera = 1)
+// }else{
+//     console.log(countera = 0)
+// }
+
+// if (b > 0){
+//     console.log(counterb = 1)
+// }else{
+//     console.log(counterb = 0)
+// }
+
+// if (c > 0){
+//     console.log(counterc = 1)
+// }else{
+//     console.log(counterc = 0)
+// }
+
+// alert(counterd = countera + counterb + counterc)
+
+
+
+
+
+
+// let a = parseInt(prompt("birinchi son"));
+// let b = parseInt(prompt("ikkinchi son"));
+
+
+// if (a > b){
+//     alert(`${a} katta ${b} dan`)
+// }else{
+//     alert(`${b} katta ${a} dan`)
+// }
+
+
+
+
+
+
+
+
+// let a = parseInt(prompt("birinchi son"));
+// let b = parseInt(prompt("ikkinchi son"));
+
+
+// if (a > b){
+//     alert(a)
+//     alert(b)
+// }else{
+//     alert(b)
+//     alert(a)
+// }
+
+
+
+
+
+
+
+
+
+// let A = parseInt(prompt("birinchi son"));
+// let B = parseInt(prompt("ikkinchi son"));
+
+
+// if (A != B){
+//     alert(`${A + B} ikki son yigindisi`)
+// }else{
+//     alert(0)
+// }
+
+// alert(`${A} birinchi son`)
+// alert(`${B} ikkinchi son`)
+
+
+
+
+
+
+
+
+let A = parseInt(prompt("birinchi son"));
+let B = parseInt(prompt("ikkinchi son"));
+
+
+if (A != B){
+    if (A > B){
+        alert(`${A} katta`)
+    }else{
+        alert(`${B} katta`)
+    }
+}else{
+    alert(0)
+}
+
+alert(`${A} birinchi son`)
+alert(`${B} ikkinchi son`)
+
+
+
+
+
+
+
+// let a = parseInt(prompt("birinchi sonni kiriting"));
+// let b = parseInt(prompt("ikkinchi sonni kiriting"));
+// let c = parseInt(prompt("uchinchi sonni kiriting"));
+
+
+
+// if (a > b){
+//     if (b >c){
+//         alert(c)
+//     }else{
+//         alert(b)
+//     }
+// }else{
+//     if (a > c){
+//         alert(c)
+//     }else{
+//         alert(a)
+//     }
+// }
+
+
+
+
+
+
+
+
