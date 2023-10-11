ecf3960ac54a6add4f063910fc34b4ec4a75bfca
7777-nuriddin <ismatullayevnuriddin7@gmail.com>
2023-10-11 11:03:51 +0500

    homework_27

---
+++ homework_27/main27.js
@@ -0,0 +1,221 @@
+
+// FOR - 1 
+
+// let n = parseInt(prompt("n ni kiriting"))
+// let k = parseInt(prompt("k ni kiriting"))
+
+// for (i = 1 ; i <= n; i++){
+
+//     alert(k)
+// }
+
+
+
+
+
+
+
+// FOR- 2
+
+
+// let a = parseInt(prompt("a ni kiriting"))
+// let b = parseInt(prompt("b ni kiriting"))
+
+
+// for (i = a; i <= b; i++){
+//     if (a < b){
+//         alert(i)
+//     }
+// }
+
+
+
+
+// FOR-3
+
+
+// let a = parseInt(prompt("a ni kiriting"))
+// let b = parseInt(prompt("b ni kiriting"))
+
+
+// for (i = b; i >= a; i--){
+//     if (a < b){
+//         alert(i)
+//     }
+// }
+
+
+
+
+
+// FOR-4
+
+
+// let N = parseInt(prompt("bir kg konfet narxi"))
+
+// for (i = 1; i <= 10; i++){
+    
+    
+//     res = N * i;
+
+//     alert(res);
+// }
+
+
+
+
+
+
+// FOR-5
+
+// let N = parseInt(prompt("bir kg konfet narxi"))
+
+// for (i = 0.1; i <= 1; i = i + 0.1){
+    
+    
+//     res = N * i;
+
+//     alert(res);
+// }
+
+
+
+
+
+
+
+
+
+// FOR-6
+
+
+
+// let N = parseInt(prompt("bir kg konfet narxi"))
+
+// for (i = 1.2; i <= 2; i = i + 0.2){
+    
+    
+//     res = N * i;
+
+//     alert(res);
+// }
+
+
+
+
+
+
+// FOR-7
+
+
+// let a = parseInt(prompt("a ni kiriting"))
+// let b = parseInt(prompt("b ni kiriting"))
+// let sum = 0
+
+// for (i = a; i <= b; i++){
+//     if (a < b){
+
+//         sum = sum + i;
+
+//     }
+// }
+// alert(sum)
+
+
+
+
+
+// FOR-8
+
+
+
+// let a = parseInt(prompt("a ni kiriting"))
+// let b = parseInt(prompt("b ni kiriting"))
+// let multiple = 1
+
+// for (i = a; i <= b; i++){
+//     if (a < b){
+
+//         multiple = multiple * i;
+
+//     }
+// }
+// alert(multiple)
+
+
+
+
+
+
+// FOR-9
+
+
+
+// let a = parseInt(prompt("a ni kiriting"))
+// let b = parseInt(prompt("b ni kiriting"))
+// let sum = 0
+
+// for (i = a; i <= b; i++){
+//     if (a < b){
+
+//         sum = sum + i*i;
+
+//     }
+// }
+// alert(sum)
+
+
+
+
+
+
+// FOR-10
+
+
+// let n = parseInt(prompt("n ni kiriting"))
+// let sum = 0
+
+// for (i = 1; i <= n; i++){
+
+//     sum = sum + 1/i
+// }
+
+// alert(sum)
+
+
+
+
+
+// FOR-11
+
+
+// let n = parseInt(prompt("n ni kiriting"))
+// let sum = 0
+
+// for (i = n; i <= 2*n; i++){
+
+//     sum = sum + i*i
+// }
+
+// alert(sum)
+
+
+
+
+
+// FOR-12
+
+
+
+let n = parseInt(prompt("n ni kiriting"))
+let multiple = 1
+
+for (i = 1.1; i <= n; i = i + 0.1){
+    
+
+        multiple = multiple * i;
+
+    
+}
+alert(multiple)
+
